d5542974f8497e15417a48642aa2c0947cac5f6e
LcukyBoy <lucas.courteaud@epitech.eu>
2022-05-24 18:38:51 +0400

    feat(codemirror): add autoCompletion and detection language between html and js

--- editor.js
+++ editor.js
@@ -1,9 +1,37 @@
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+import {EditorState, Compartment} from "@codemirror/state"
+import {language} from "@codemirror/language"
+import {htmlLanguage, html} from "@codemirror/lang-html"
+import {php} from "@codemirror/lang-php"
+import {css} from "@codemirror/lang-css"
 import {javascript} from "@codemirror/lang-javascript"
+import {sql} from "@codemirror/legacy-modes/mode/sql"
+import {EditorView, basicSetup} from "@codemirror/basic-setup"
 
-let editor = new EditorView({
-  state: EditorState.create({
-    extensions: [basicSetup, javascript()]
-  }),
-  parent: document.body
+const languageConf = new Compartment
+
+const autoLanguage = EditorState.transactionExtender.of(tr => {
+  if (!tr.docChanged) {
+    return null
+  }
+  let docIsHTML = /^\s*</.test(tr.newDoc.sliceString(0, 100))
+  let stateIsHTML = tr.startState.facet(language) == htmlLanguage
+  if (docIsHTML == stateIsHTML) {
+    return null
+  }
+  return {
+    effects: languageConf.reconfigure(docIsHTML ? html() : javascript())
+  }
 })
+
+window.codemirror = {
+  EditorView: EditorView,
+  EditorState: EditorState,
+  basicSetup: basicSetup,
+  html: html,
+  php: php,
+  css: css,
+  javascript: javascript,
+  sql: sql,
+  autoLanguage: autoLanguage,
+  languageConf: languageConf
+ };
