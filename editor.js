a535213d31b42b98f5907d0fa44922ebda2b76c2
LcukyBoy <lucas.courteaud@epitech.eu>
2022-05-25 17:26:16 +0400

    feat(codemirror): add languages in editor

--- editor.js
+++ editor.js
@@ -4,7 +4,12 @@ import {htmlLanguage, html} from "@codemirror/lang-html"
 import {php} from "@codemirror/lang-php"
 import {css} from "@codemirror/lang-css"
 import {javascript} from "@codemirror/lang-javascript"
+import {json} from "@codemirror/lang-json"
+import {markdown} from "@codemirror/lang-markdown"
+import {xml} from "@codemirror/lang-xml"
 import {sql} from "@codemirror/legacy-modes/mode/sql"
+import {shell} from "@codemirror/legacy-modes/mode/shell"
+
 import {EditorView, basicSetup} from "@codemirror/basic-setup"
 
 const languageConf = new Compartment
@@ -31,7 +36,11 @@ window.codemirror = {
   php: php,
   css: css,
   javascript: javascript,
+  json: json,
+  markdown: markdown,
+  xml: xml,
   sql: sql,
+  shell: shell,
   autoLanguage: autoLanguage,
   languageConf: languageConf
  };
