0201e66e0240ad61373f3926dfa9de585c6965ac
LcukyBoy <lucas.courteaud@epitech.eu>
2022-06-15 19:29:37 +0400

    delete test-theme.js and add themes from cm6-theme

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-./node_modules/.bin/rollup test-theme.js -f iife -o editor.component.js -p @rollup/plugin-node-resolve
+./node_modules/.bin/rollup editor.js -f iife -o editor.component.js -p @rollup/plugin-node-resolve
--- editor.js
+++ editor.js
@@ -1,53 +1,98 @@
-import { StreamLanguage, defaultHighlightStyle } from "@codemirror/language"; 
+import * as view from "@codemirror/view"
+import * as state from "@codemirror/state"
+import * as language from "@codemirror/language"
+import * as commands from "@codemirror/commands"
+import * as search from "@codemirror/search"
+import * as autocomplete from "@codemirror/autocomplete"
+import * as lint from "@codemirror/lint"
+import * as html from "@codemirror/lang-html"
+import * as php from "@codemirror/lang-php"
+import * as css from "@codemirror/lang-css"
+import * as javascript from "@codemirror/lang-javascript"
+import * as json from "@codemirror/lang-json"
+import * as markdown from "@codemirror/lang-markdown"
+import * as xml from "@codemirror/lang-xml"
+import * as sql from "@codemirror/legacy-modes/mode/sql"
+import { basicLight } from "cm6-theme-basic-light"
+import { oneDark } from "@codemirror/theme-one-dark"
+import { solarizedLight } from "cm6-theme-solarized-light"
+import { solarizedDark } from "cm6-theme-solarized-dark"
+import { materialDark } from "cm6-theme-material-dark"
+import { nordTheme } from "cm6-theme-nord"
+import { gruvboxLight } from "cm6-theme-gruvbox-light"
+import { gruvboxDark } from "cm6-theme-gruvbox-dark"
 
-import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
-import { autocompletion } from "@codemirror/autocomplete";
-
-import {keymap} from '@codemirror/view'
-import {indentWithTab} from "@codemirror/commands";
-import {lintGutter, linter} from "@codemirror/lint"
-import Linter from "eslint4b-prebuilt"
-
-import {html} from "@codemirror/lang-html"
-import {php} from "@codemirror/lang-php"
-import {css} from "@codemirror/lang-css"
-import {esLint, javascript} from "@codemirror/lang-javascript"
-import {json} from "@codemirror/lang-json"
-import {markdown} from "@codemirror/lang-markdown"
-import {xml} from "@codemirror/lang-xml"
-import {sql} from "@codemirror/legacy-modes/mode/sql"
-import {shell} from "@codemirror/legacy-modes/mode/shell"
-
-import { oneDark, oneDarkTheme, oneDarkHighlightStyle } from "@codemirror/theme-one-dark";
-
-import {themes} from "./themes/themes"
-
-const dark = [oneDarkTheme, oneDarkHighlightStyle];
+const languageExtensions = {
+    javascript: [new language.LanguageSupport(javascript.javascriptLanguage)],
+    html: [new language.LanguageSupport(html.htmlLanguage)],
+    css: [new language.LanguageSupport(css.cssLanguage)],
+    php: [php.php()],
+    json: [new language.LanguageSupport(json.jsonLanguage)],
+    sql: [language.StreamLanguage.define(sql.mySQL)],
+    md: [new language.LanguageSupport(markdown.markdownLanguage)],
+}
+  
+const themeExtensions = {
+    basicLight: basicLight,
+    basicDark: oneDark,
+    solarizedLight: solarizedLight,
+    solarizedDark: solarizedDark,
+    materialDark: materialDark,
+    nordTheme: nordTheme,
+    gruvboxLight: gruvboxLight,
+    gruvboxDark: gruvboxDark
+}
 
 window.codemirror6 = {
-  EditorView: EditorView,
-  EditorState: EditorState,
-  basicSetup: basicSetup,
-  autocompletion: autocompletion,
+  view: view,
+  state: state,
+  language: language,
+  commands: commands,
+  search: search,
+  autocomplete: autocomplete,
+  lint: lint,
   html: html,
   php: php,
   css: css,
   javascript: javascript,
   json: json,
-  markdown: markdown,
   xml: xml,
+  markdown: markdown,
   sql: sql,
-  shell: shell,
-  StreamLanguage: StreamLanguage,
-  defaultHighlightStyle: defaultHighlightStyle,
-  keymap: keymap,
-  indentWithTab: indentWithTab,
-  linter: linter,
-  esLint: esLint,
-  Linter: Linter,
-  lintGutter: lintGutter,
-  themes: themes,
-  oneDarkTheme: oneDarkTheme,
-  oneDark: oneDark,
-  dark: dark
+  languageExtensions: languageExtensions,
+  themeExtensions: themeExtensions,
+  getBasicExtensions() {
+    let cm = window.codemirror6;
+    if (!cm.ext) {
+      cm.ext = {
+        lineNumbers: cm.view.lineNumbers(),
+        highlightActiveLineGutter: cm.view.highlightActiveLineGutter(),
+        highlightSpecialChars: cm.view.highlightSpecialChars(),
+        history: cm.commands.history(),
+        foldGutter: cm.language.foldGutter(),
+        drawSelection: cm.view.drawSelection(),
+        dropCursor: cm.view.dropCursor(),
+        allowMultipleSelections: cm.state.EditorState.allowMultipleSelections.of(true),
+        indentOnInput: cm.language.indentOnInput(),
+        syntaxHighlighting: cm.language.syntaxHighlighting(cm.language.defaultHighlightStyle, {fallback: true}),
+        bracketMatching: cm.language.bracketMatching(),
+        closeBrackets: cm.autocomplete.closeBrackets(),
+        autocompletion: cm.autocomplete.autocompletion(),
+        rectangularSelection: cm.view.rectangularSelection(),
+        crosshairCursor: cm.view.crosshairCursor(),
+        highlightActiveLine: cm.view.highlightActiveLine(),
+        highlightSelectionMatches: cm.search.highlightSelectionMatches(),
+        keymap: cm.view.keymap.of([
+          ...cm.autocomplete.closeBracketsKeymap,
+          ...cm.commands.defaultKeymap,
+          ...cm.search.searchKeymap,
+          ...cm.commands.historyKeymap,
+          ...cm.language.foldKeymap,
+          ...cm.autocomplete.completionKeymap,
+          ...cm.lint.lintKeymap
+        ]),
+      };
+    }
+    return cm.ext;
+  }
 };
